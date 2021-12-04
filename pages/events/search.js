7e72dbbfd694ceb4fbe19d00bc3609988f8b49c6
rishavjaiss <rishavjaiss14@gmail.com>
2021-12-04 13:09:24 +0530

    Added Search Events Functionality

---
+++ pages/events/search.js
@@ -0,0 +1,38 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+import qs from "qs";
+import EventItem from "@/components/EventItem";
+import Layout from "@/components/layout";
+import { API_URL } from "@/config/index";
+
+export default function SearchPage({ events }) {
+  const router = useRouter();
+  return (
+    <Layout title="Search Results">
+      <Link href="/events">Go Back</Link>
+      <h1>Search Results for {router.query.term}</h1>
+      {events.length === 0 && <h3>No Events to show.</h3>}
+      {events.map((event) => (
+        <EventItem key={event.id} event={event} />
+      ))}
+    </Layout>
+  );
+}
+
+export async function getServerSideProps({ query: { term } }) {
+  const query = qs.stringify({
+    _where: {
+      _or: [
+        { name_contains: term },
+        { description_contains: term },
+        { performers_contains: term },
+        { venue_contains: term },
+      ],
+    },
+  });
+  const res = await fetch(`${API_URL}/events?${query}`);
+  const events = await res.json();
+  return {
+    props: { events },
+  };
+}
