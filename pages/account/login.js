fdad08e02093f60baa6aeb5d29825a8652d70f1d
rishavjaiss <rishavjaiss14@gmail.com>
2021-12-10 23:35:17 +0530

    escaped single quotes with entities

--- pages/account/login.js
+++ pages/account/login.js
@@ -48,7 +48,8 @@ export default function LoginPage() {
           <input type="submit" className="btn" value="Login" />
         </form>
         <p>
-          Don't have an account? <Link href="/account/register">Register</Link>
+          Don&apos;t have an account?
+          <Link href="/account/register">Register</Link>
         </p>
       </div>
     </Layout>
